6cc26514a301a713f84a5a4d62498f5dfca949e2
SamuelJRich <120282121+SamuelJRich@users.noreply.github.com>
2024-10-03 16:03:41 +0100

    Created the main URL Mapping in the project for the already created home app mapping.

--- _greentrail/home/urls.py
+++ _greentrail/home/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views # Imports the file called 'views' from the current directory.
 
-urlMappings = [
+urlpatterns = [
     path('homepage/', views.home, name='homepage'),
 ]
