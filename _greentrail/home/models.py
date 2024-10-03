d580756e8817aeec501ba14ac4e8c7debc753041
SamuelJRich <120282121+SamuelJRich@users.noreply.github.com>
2024-10-03 16:19:12 +0100

    Created a Signup class for the App (database table)

--- _greentrail/home/models.py
+++ _greentrail/home/models.py
@@ -2,3 +2,12 @@ from django.db import models
 
 # Create your models here.
 
+class Signup(models.Model):
+    
+    forename = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+    dob = models.DateField()
+    
+    
+    
