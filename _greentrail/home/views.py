8d490e074dccd81c9650e81f3ea215a3861afa6d
SamuelJRich <120282121+SamuelJRich@users.noreply.github.com>
2024-10-04 17:26:02 +0100

    Configured the correct imports, and added a test function for checking if the API works,.

--- _greentrail/home/views.py
+++ _greentrail/home/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 def home(request):
     return HttpResponse("This will be the landing page!")
+
+@api_view(['GET'])
+def api_test(request):
+    data = {
+        "message": "API is working!",  # Just a simple test message
+    }
+    return Response(data)
